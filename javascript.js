93673a510b80dfb0eedfbee4b5764064ee7a0be7
gredman2018 <43188682+gredman2018@users.noreply.github.com>
2020-03-03 19:54:59 -0500

    created the slideshow on homepage

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function cycleBackgrounds(interval) {
+  let index = 0
+
+  const $imageEls = $('.container .slide') // Get the images to be cycled.
+
+  setInterval(() => {
+    // Get the next index.  If at end, restart to the beginning.
+    index = index + 1 < $imageEls.length ? index + 1 : 0
+
+    // Show the next
+    $imageEls.eq(index).addClass('show')
+
+    // Hide the previous
+    $imageEls.eq(index - 1).removeClass('show')
+  }, interval)
+}
+
+$(function () {
+  cycleBackgrounds(2000);
+});
